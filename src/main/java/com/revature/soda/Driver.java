e0c91f39ae43ed3282f90216c0e2a3c254ebebab
Carlos Santos <csantos2@uncc.edu>
2019-08-28 11:52:49 -0400

    Updated Driver class

--- src/main/java/com/revature/soda/Driver.java
+++ src/main/java/com/revature/soda/Driver.java
@@ -4,7 +4,9 @@ public class Driver {
 
 	public static void main(String[] args) {
 		SoftDrink a = new Cola();
-		System.out.println(a.toString() + "Fizz? " + a.getFizz());
+		SoftDrink b = new Ginger();
+		System.out.println(a.toString() + " Fizz? " + a.getFizz());
+		System.out.println(b.toString() + " Fizz? " + b.getFizz() + " Is ginger raw? " + ((Ginger)b).isGingerRaw());
 	}
 
 }
